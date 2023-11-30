c439ec332220b00ed93e02e4b565e10916270c49
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-11-30 01:59:16 -0800

    test&

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import hidden_4
 
-    import  hidden_4
-    for i in sorted(dir(hidden_4)):
-        if i[:2] != "__":
-            print("{}".format(i))
+    for nam in sorted(dir(hidden_4)):
+
+        if nam[:2] != '__':
+            print("{}".format(nam))
