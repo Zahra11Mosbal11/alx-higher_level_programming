62ae1595e0ad54eef3e3df7364caf26037510814
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-12-02 01:50:08 -0800

    new elment

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return
+    len_list = len(my_list)
+    if idx > len_list - 1:
+        return
+    my_list[idx] = element
+    return (my_list)
