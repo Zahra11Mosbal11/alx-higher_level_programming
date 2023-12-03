cf3c82337e239488caebc8150583050a2f39d47b
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-12-02 21:08:50 -0800

    max

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len_str  = len(sentence)
+    len_str = len(sentence)
     first = sentence[0] if len_str > 0 else "None"
     tu = len_str, first
     return (tu)
