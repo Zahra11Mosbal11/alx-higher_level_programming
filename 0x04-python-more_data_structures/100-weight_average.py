713e162800033c1558afdefcdc7fa8943bee9107
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-12-06 10:28:42 -0800

    done

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if not my_list:
         return 0
     num = sum(x * y for x, y in my_list)
     denm = sum(y for _, y in my_list)
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if len(roman_string) == 1:
-        return roman.get(roman_string, 0)
     total_roman = 0
     for i in range(len(roman_string) - 1):
         if roman[roman_string[i]] < roman[roman_string[i + 1]]:
