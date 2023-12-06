197a4a6015476394c23dadf9bba9dd1c1e36f9e1
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-12-06 05:44:29 -0800

    last 3

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    i = 0
+    for x in a_dictionary:
+        if i < a_dictionary[x]:
+            i = a_dictionary[x]
+    for x in a_dictionary:
+        if i == a_dictionary[x]:
+            return x
