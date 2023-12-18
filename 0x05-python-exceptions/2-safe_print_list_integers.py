d0f8fdf3f0a68c374c415b7fb01fcdbb72cb48af
Zahra11Mosbal <zahramosbal@gmail..com>
2023-12-18 03:21:44 -0800

    test

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    len_l = 0
+    les = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            len_l += 1
+            les += 1
         except (ValueError, TypeError):
-           continue 
+            continue
     print("")
-    return (len_l)
+    return (les)
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     res = []
     for i in range(0, list_length):
         try:
-            div = my_list_1[i] / my_list_2[i]
+            div_lists = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             div_lists = 0
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             div_lists = 0
         finally:
-            res.append(div)
+            res.append(div_lists)
     return res
