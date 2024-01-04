16b2b9588728add4e8bbd7f863cbef7ab13d2fb1
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-01-04 02:18:23 -0800

    test

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -69,7 +69,9 @@ class Rectangle:
             return 0
         return ((2 * self.__width) + (2 * self.__height))
     def __str__(self):
-        """Should print the rectangle with the character #"""
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
+        """
         if self.__width == 0 or self.__height == 0:
             return ("")
 
