d58f7eb52b338ee87a74d1feb3e5b95d7cf4d868
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-01-08 19:16:07 -0800

    done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    def inherits_from(obj, a_class):
     """Returns true if obj is a subclass of a_class, otherwise false"""
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Will contains 6-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Class will have public attribute area"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
