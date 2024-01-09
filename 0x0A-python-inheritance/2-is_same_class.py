7c3521982bffec8433054cb7c57ff89b0b493bf6
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-01-09 03:58:58 -0800

    test

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""This will check if the object is exactly an instance of the specified class"""
+'''This will check if the object is exactly an instance of the specified class'''
 
 
 def is_same_class(obj, a_class):
-    """Return True if the object is exactly an instance of the specified
-    otherwise False
-    """
+    '''Return True if the object is exactly an instance of the specified'''
     return (type(obj) == a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Contains 4-inherits_from.py"""
+'''Contains 4-inherits_from.py'''
 
 
 def inherits_from(obj, a_class):
-    """Returns true if obj is a subclass of a_class, otherwise false"""
+    '''Returns true if obj is a subclass of a_class, otherwise false'''
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
