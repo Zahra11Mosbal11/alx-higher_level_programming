9453dd168e8360816329b56fde4abb4989b9fed5
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-01-09 10:34:02 -0800

    test

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """Methid that returns a list of lists of integer Pascal’s triangle"""
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
+
