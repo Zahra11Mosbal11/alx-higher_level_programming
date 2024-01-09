6a3d411f62204564800b172d5c913087c2c7d3bb
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-01-09 09:41:55 -0800

    test

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Function that returns the dictionary description with simple data"""
-import json
 
 
 def class_to_json(obj):
     """method that convert JSON serialization obj to dictionary"""
-    data = obj.__dict__
-    return data
+    return obj.__dict__
