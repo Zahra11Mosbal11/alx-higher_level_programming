8c9b558e8db0e8d02719ae7b2f73005db0eb1cf0
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-01-09 09:56:58 -0800

    do

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Contains 9-student.py"""
+
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """That retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
