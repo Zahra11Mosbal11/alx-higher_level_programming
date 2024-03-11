e136d4dff8e719aab755a09367afb5ddd7ea36b7
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-03-11 11:35:41 -0700

    test

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-let i;
-let j;
-const x = process.argv[2];
-
-if (x === undefined || isNaN(parseInt(x))) {
-  console.log('Missing or invalid size');
+#!/usr/bin/node
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing size');
 } else {
-  for (i = 0; i < parseInt(x); i++) {
+  for (let r = 0; r < x; r++) {
     let row = '';
-    for (j = 0; j < parseInt(x); j++) {
-      row += 'X ';
-    }
+    for (let c = 0; c < x; c++) row += 'X';
     console.log(row);
   }
 }
