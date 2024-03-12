b124a373773756a0c45eee6407d55c227178900a
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-03-12 12:49:15 -0700

    test

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,9 @@
+exports.converter = function (base) {
+  return function convert (num) {
+    if (num === 0) {
+      return '';
+    } else {
+      return convert(Math.floor(num / base)) + (num % base).toString(base).toUpperCase();
+    }
+  };
+};
