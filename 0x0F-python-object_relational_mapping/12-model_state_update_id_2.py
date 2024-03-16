7c72a43445349574df624c7cb207bab1b26e3a63
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-03-15 23:59:35 -0700

    test

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """not be executed when imported"""
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(
+            f"mysql://{username}:{password}@localhost:3306/{database_name}"
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
