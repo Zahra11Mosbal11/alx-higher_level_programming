84a30e6b416d5046a5ce355f16083b599a43e571
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-03-28 05:42:10 -0700

    tone

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Response header value #0"""
-import urllib.request
-import sys
-
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    request_id = response.headers.get('X-Request-Id')
-print(request_id)
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        request_id = response.headers.get('X-Request-Id')
+    print(request_id)
