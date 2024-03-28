c08fff469df9b2538bbb4670583bed1646699f8c
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-03-28 10:27:03 -0700

    try

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Error code """
+if __name__ == "__main__":
+    import requests
+    import sys
+    req = requests.get(sys.argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
