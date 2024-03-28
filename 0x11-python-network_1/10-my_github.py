c4969f45d2960aa48f9072b24fd633567e5b281a
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-03-28 11:07:11 -0700

    try

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,10 +2,9 @@
 """My GitHub!"""
 import requests
 import sys
-
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    req = requests.get('https://api.github.com/user', auth=(username, password))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get('https://api.github.com/user', auth=auth)
     print(req.json().get("id"))
